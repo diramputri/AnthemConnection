8d9c74bd4618fc2c7e9d4fe0b639484487d701de
Harnoor Singh <hsingh9@student.gsu.edu>
2018-03-24 13:39:28 -0400

    Drawable files added

---
+++ app/src/main/java/com/iharnoor/anthemconnection/Questions.kt
@@ -0,0 +1,12 @@
+package com.iharnoor.anthemconnection
+
+val listQuestions = listOf<String>("I feel like a valuable team member at Anthem:",
+        "My work environment allows me to make progress at my job:",
+        "I am comfortable asking coworkers and supervisors for help:",
+        "If I get upset at work, I know who to talk to:",
+        "I feel safe around my coworkers:",
+        "tWhen my coworkers are unhappy, I know how to help them:",
+        "I rarely have issues working with groups:",
+        "I feel like work is distributed evenly and fairly in my group projects:",
+        "Goals are made faster when working in a group:",
+        "I tend to be the leader of my group projects:")
--- app/src/main/java/com/iharnoor/anthemconnection/Survey.kt
+++ app/src/main/java/com/iharnoor/anthemconnection/Survey.kt
@@ -1,7 +1,10 @@
 package com.iharnoor.anthemconnection
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import kotlinx.android.synthetic.main.activity_survey.*
 
 class Survey : AppCompatActivity() {
@@ -9,12 +12,48 @@ class Survey : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_survey)
+        selectedFace.visibility = View.INVISIBLE
+        skipOrNextBtn.setBackgroundColor(Color.GRAY)
 
 
-        val listQuestions= listOf<String>("")
+
         closeBtn.setOnClickListener {
-
             finish()
+        }
+        agree.setOnClickListener {
+            selectedFace.visibility = View.VISIBLE
+            skipOrNextBtn.setBackgroundColor(Color.BLUE)
+            selectedFace.setImageResource(R.drawable.slightly_smiling_face_emoji)
+            skipOrNextBtn.setText("NEXT")
+        }
+        stronglyDisagree.setOnClickListener {
+            selectedFace.visibility = View.VISIBLE
+            skipOrNextBtn.setBackgroundColor(Color.BLUE)
+            skipOrNextBtn.setText("NEXT")
+            selectedFace.setImageResource(R.drawable.very_sad_emoji_icon_png)
+        }
+        stronglyAgree.setOnClickListener {
+            skipOrNextBtn.setBackgroundColor(Color.BLUE)
+            skipOrNextBtn.setText("NEXT")
+            selectedFace.visibility = View.VISIBLE
+            selectedFace.setImageResource(R.drawable.smiling_emoji_with_eyes_opened)
+        }
+        neutralFace.setOnClickListener {
+            skipOrNextBtn.setBackgroundColor(Color.BLUE)
+            skipOrNextBtn.setText("NEXT")
+            selectedFace.visibility = View.VISIBLE
+            selectedFace.setImageResource(R.drawable.neutral_face_emoji)
+        }
+        disagree.setOnClickListener {
+            skipOrNextBtn.setBackgroundColor(Color.BLUE)
+            skipOrNextBtn.setText("NEXT")
+            selectedFace.visibility = View.VISIBLE
+            selectedFace.setImageResource(R.drawable.confused_face_emoji)
+        }
+        selectedFace.setOnClickListener {
+            selectedFace.visibility = View.INVISIBLE
+            skipOrNextBtn.setBackgroundColor(Color.GRAY)
+            skipOrNextBtn.setText("SKIP")
         }
     }
 }
